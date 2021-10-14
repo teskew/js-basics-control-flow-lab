f35b7777397dbc304eb4a4cc5dda563ef607fe73
github username <github email address>
2021-10-14 16:34:40 -0500

    commit tree cf6ebd6183ed60845165d9e2de73fe15a5b978df

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result
+
+  if (someValue <= 400) {
+
+   return 'This one is on me!';
+
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+
+    return   'I will gladly take your thirty bucks.'
+
+  }
+  else if  (someValue > 2500) {
+
+    return  'No can do.';
+  }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  
+
+   if (city === 'NYC') {
+
+    return "Ok, sounds good." 
+   }
+   else {
+     return 'No go.'
+   }
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
